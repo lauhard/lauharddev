6f9a3de23f95f2869a1bfd476fb2655d1661cb59
lauhard <lauhard@proton.me>
2024-10-22 22:23:48 +0200

    update: redirect from root page to blog page

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,10 @@
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ cookies, url }) => {
     const _theme = cookies.get('data-theme') || "";
+    console.log("url", url)
+    if (url.pathname == '/') redirect(307, '/blog');
     return {
         theme: _theme,
         url: url.pathname
