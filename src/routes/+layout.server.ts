1557bc7a70982ec87ad29adf5129f07095438a5f
lauhard <lauhard@proton.me>
2024-10-28 23:40:20 +0100

    fix: data-theme cookie name -> to get correct cookie from server

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,7 @@ import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 //export const prerender = true;
 export const load: LayoutServerLoad = async ({ cookies, url }) => {
-    const _theme = cookies.get('data-theme') || "";
+    const _theme = cookies.get('lauhard.dev-data-theme') || "";
     if (url.pathname == '/') redirect(307, '/blog');
     return {
         theme: _theme,
