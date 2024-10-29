97bf44aa2aa33711b2bd47487b5a660983e9330e
lauhard <lauhard@proton.me>
2024-10-29 23:41:16 +0100

    fix: fixed footer svgs and links

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,6 @@
 <script>
     import { dev } from "$app/environment";
+    import { codepen, github, twitter } from "$lib/project.config";
     import { Github, GithubIcon, LucideGithub } from "lucide-svelte";
 </script>
 
@@ -7,10 +8,10 @@
     <div class="footer-content">
         <div class="social-buttons">
             <a
-                href="https://www.hp.com"
+                href={github}
                 target="_blank"
                 class="social-button hp-facebook"
-                aria-label="Facebook"
+                aria-label="Github"
             >
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -29,10 +30,10 @@
                 >
             </a>
             <a
-                href="https://www.hp.com"
+                href={codepen}
                 target="_blank"
                 class="social-button hp-instagram"
-                aria-label="Instagram"
+                aria-label="Codepen"
             >
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -58,26 +59,30 @@
                 >
             </a>
             <a
-                href="https://www.hp.com"
+                href={twitter}
                 target="_blank"
                 class="social-button hp-linkedin"
-                aria-label="LinkedIn"
+                aria-label="X (Twitter)"
             >
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
                     height="24"
-                    viewBox="0 0 24 24"
+                    viewBox="0 0 90 90"
                     fill="none"
                     stroke="currentColor"
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     class="lucide lucide-twitter"
-                    ><path
-                        d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"
-                    /></svg
                 >
+                    <path
+                        d="M 0.219 2.882 l 34.748 46.461 L 0 87.118 h 7.87 l 30.614 -33.073 l 24.735 33.073 H 90 L 53.297 38.043 L 85.844 2.882 h -7.87 L 49.781 33.341 L 27.001 2.882 H 0.219 z M 11.793 8.679 h 12.303 L 78.425 81.32 H 66.122 L 11.793 8.679 z"
+                        style="stroke-width: 4;  stroke-linecap: round; stroke-linejoin: round; opacity: 1;"
+                        transform=" matrix(1 0 0 1 0 0) "
+                        stroke-linecap="round"
+                    />
+                </svg>
             </a>
         </div>
         <p>&copy; Lauhard.dev. All rights reserved.</p>
